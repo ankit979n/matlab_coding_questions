210053c1c202c8953cce040238e99c6f7943705f
ankit979n <95979600+ankit979n@users.noreply.github.com>
2023-12-24 21:20:56 +0530

    matrix operations

---
+++ A1_7.m
@@ -0,0 +1,30 @@
+%Use matlab to find addition, substraction and multiplication of 2D matrix.
+
+% Define two 2D matrices
+matrix1 = [1, 2, 3; 4, 5, 6; 7, 8, 9];
+matrix2 = [9, 8, 7; 6, 5, 4; 3, 2, 1];
+
+% Addition of matrices
+addition_result = matrix1 + matrix2;
+
+% Subtraction of matrices
+subtraction_result = matrix1 - matrix2;
+
+% Multiplication of matrices
+multiplication_result = matrix1 * matrix2;
+
+% Display the results
+disp('Matrix 1:');
+disp(matrix1);
+
+disp('Matrix 2:');
+disp(matrix2);
+
+disp('Addition Result:');
+disp(addition_result);
+
+disp('Subtraction Result:');
+disp(subtraction_result);
+
+disp('Multiplication Result:');
+disp(multiplication_result);
