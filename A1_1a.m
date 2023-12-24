fc57bc44ded0fbd3c89130fa47694ad264bd4a9a
ankit979n <95979600+ankit979n@users.noreply.github.com>
2023-12-24 21:15:49 +0530

    Basic arithmatic problems

---
+++ A1_1a.m
@@ -0,0 +1,5 @@
+%1.Write a program to assign the following expression to a variable A and then to print out the value of A:
+%a) 3+4 / 5+6
+
+A=(3+4)/(5+6);
+disp(A);
---
+++ A1_2a.m
@@ -0,0 +1,6 @@
+%2. Set up a vector called N with 5 elements having the values  1,2,3,4,5. Using N, create assignment statements for a vector X which will result in X having these values: 
+%a) 2,4,6,8,10
+
+N = [1, 2, 3, 4, 5];
+X = 2 * N;
+disp(A);
